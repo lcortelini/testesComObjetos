402dc81e3bfa7674213a780c853decc407d0253c
lararson.cortelini <lmcortelini@gmail.com>
2021-03-09 16:04:09 -0300

    doubleArray

---
+++ app/src/main/java/one/digitalinnovation/collections/TesteDoubleArray.kt
@@ -0,0 +1,25 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val salarios = DoubleArray(3)
+    salarios[0] = 1000.0
+    salarios[1] = 3000.0
+    salarios[2] = 2000.0
+
+    salarios.sort()
+    salarios.forEach{println(it)}
+
+    println("--------------")
+    salarios.forEachIndexed { index, valorDoIndice ->
+        salarios[index] = valorDoIndice * 1.1
+    }
+
+    salarios.forEach{println(it)}
+
+
+    println("--------------")
+    val salarios2 = doubleArrayOf(1500.0, 1250.0, 5000.0)
+
+    salarios2.sort()
+    salarios2.forEach{ println(it) }
+}
